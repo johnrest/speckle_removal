0e38a562c0016d53ba7204cf09c1653b1633e463
John <jfrestre@gmail.com>
2018-10-30 20:29:29 -0500

    added a script to process the holograms with a fcn sampled mask.
    Added functionality to creat DMD patterns combining a linear carrier
    with a FCN sampled mask...

--- scripts/generate_mask.py
+++ scripts/generate_mask.py
@@ -1,20 +1,21 @@
 from speck_rem.dmd import *
 
-target_folder = "C:/Users/itm/Desktop/DH/2018_10_17/test"
+target_folder = "C:/Users/itm/Desktop/DH/2018_10_25/test"
 mask_image_prefix = "pattern_"
 
 # Select period values in pixels
-period_list = np.linspace(8, 20, num=7, endpoint=True)
-# period_list = [8.0]
+# period_list = np.linspace(8, 20, num=7, endpoint=True)
+period_list = [10.0]
 
 # Select angle values
 # angle_list =  np.linspace(0.0, np.pi/2.0, num=5, endpoint=True)
-angle_list = [0.0]
+angle_list = [np.pi/4]
 
 for itr, angle in enumerate(angle_list):
     for jtr, period in enumerate(period_list):
         mask = Mask()
-        mask.compute_mask(angle, period)
+        # mask.compute_mask(angle, period)
+        mask.compute_fcn_mask(angle, period)
         print("Angle (grad): {0}; Period (pix): {1}".format(angle*180/np.pi, period))
         current_image_file = os.path.join(target_folder, mask_image_prefix + "{:03d}".format(itr+jtr))
         print("Writing image to file: ", current_image_file, )
--- speck_rem/dmd.py
+++ speck_rem/dmd.py
@@ -29,5 +29,26 @@ class Mask(Image):
         self.image_array = 1/2 + 1/2 * np.sign(temp2)
 
 
+    def compute_fcn_mask(self,theta, period):
+        grain = 32
+        pattern_size = int(1024 / grain)
+        number_pattern_images = (pattern_size * pattern_size) / 512
+        pattern_batch = compute_pattern_batch(scale=pattern_size, batch_length=number_pattern_images)
+
+        u, v = np.meshgrid(np.linspace(0, self.width, self.width, endpoint=False) + 1/2,
+                          np.linspace(0, self.height, self.height, endpoint=False) + 1/2)
+
+        temp = (u * np.round(np.cos(theta), decimals=2) + v * np.round(np.sin(theta), decimals=2))
+        mod = pattern_batch[0]
+        mod = mod.repeat(grain, axis=0).repeat(grain, axis=1)
+
+        # print(np.pad(np.ones((3,3)), ((3, 2), (2, 3)), 'constant', constant_values=(0)))
+        # pad1 = ((1080-1024)/2, (1080-1024)/2)
+        mod = np.pad(mod, ((28, 28), (448,448)), 'constant', constant_values=(0))
+
+        temp2 = np.sin((2.0 * math.pi / period) * temp - np.pi*mod)
+        self.image_array = 1 / 2 + 1 / 2 * np.sign(temp2)
+
+
 if __name__ == "__main__":
     main()
